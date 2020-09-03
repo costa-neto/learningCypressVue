e6862f3a0a1d60471b497d268fb27f972ce0cfe7
Neto Costa <costa.neto@unesp.br>
2020-09-03 18:02:25 -0300

    Primeiro Commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="login">
+    <h1>Essa é minha página de Login!</h1>
+    <label>E-mail:</label><input v-model="email" class="input-email" /> <br>
+    <label>Senha:</label><input v-model="senha" class="input-password" type="password"/><br><br>
+    <button @click="cliqueLogin">Clique em mim</button>
+    <h1 v-if="logado">Você está logado</h1>
+    <h1 v-else>Login inválido!</h1>
   </div>
 </template>
+<script>
+export default {
+  data: function(){
+    return{
+      email:'',
+      senha: '',
+      logado: false
+    }
+  },
+  methods: {
+    cliqueLogin: function()
+    {
+      if(this.email==='neto@email.com' && this.senha==='minhaSenha')
+        this.logado=true;
+      else
+        this.logado=false;
+    }
+  }
+}
+</script>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,5 +4,25 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Welcome to Your Vue.js App')
+  }),
+  it('Visits the about page', () => {
+    cy.visit('/')
+    cy.contains('Login').click()
+    cy.contains('h1','Essa é minha página de Login!')
+  }),
+  it('Faz um login inválido', () => {
+    cy.get('input.input-email').type('neto@costa.com')
+    cy.get('input.input-password').type('minhaSenha')
+    cy.get('button').click()
+    cy.contains('h1','inválido')
+  }),
+  it('Faz um login válido', () => {
+    cy.get('input.input-email').clear()
+    cy.get('input.input-password').clear()
+    cy.get('input.input-email').type('neto@email.com')
+    cy.get('input.input-password').type('minhaSenha')
+    cy.get('button').click()
+    cy.contains('h1','logado')
   })
+
 })
